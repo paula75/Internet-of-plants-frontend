b5c70355d1274ff920aa882c88f731b447cba13d
portega <portega@tracktec.cl>
2020-06-30 13:55:06 -0400

    changes in initial setup

--- internet-of-plants/src/app/app.module.ts
+++ internet-of-plants/src/app/app.module.ts
@@ -1,16 +1,39 @@
+import { Routes, RouterModule } from '@angular/router';
+import { OverviewComponent } from './overview/overview.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { TopbarComponent } from './topbar/topbar.component';
+
+export const plantsRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: 'overview',
+    pathMatch: 'full'
+  },
+  {
+    path: 'overview',
+    component: OverviewComponent
+  }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OverviewComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    RouterModule.forChild(plantsRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
